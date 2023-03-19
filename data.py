82715fb9ebef1fac179330cd6e67bb4797cc2127
Jake Watembach <jakewatembach@gmail.com>
2023-03-19 07:52:18 -0600

    final version

--- data.py
+++ data.py
@@ -19,23 +19,30 @@ df = pd.read_csv(file_path)
 df_CO = df[df['BusinessState'] == 'CO']
 df_CO['GroupedRestaurantType'] = df_CO['RestaurantType'].apply(group_restaurant_types)
 
-# Find the average
-grouped_data = df_CO.groupby('GroupedRestaurantType')['GrantAmount'].mean()
+# Find the average and count of grants for each restaurant type
+grouped_data = df_CO.groupby('GroupedRestaurantType')['GrantAmount'].agg(['mean', 'count'])
 
 N = 10 
-grouped_data = grouped_data.sort_values(ascending=True).tail(N)
+grouped_data = grouped_data.sort_values(by='mean', ascending=True).tail(N)
 
-plt.figure(figsize=(12, 8))
+fig, ax = plt.subplots(figsize=(12, 8))
+
 bar_colors = plt.cm.viridis(np.linspace(0, 1, N))
-bar_plot = plt.barh(grouped_data.index.str.title(), grouped_data.values, color=bar_colors, edgecolor='black', linewidth=1.2, alpha=0.85)
+bar_plot = ax.barh(grouped_data.index.str.title(), grouped_data['mean'], color=bar_colors, edgecolor='black', linewidth=1.2, alpha=0.85)
 
-plt.grid(axis='x', linestyle='--', alpha=0.7)
-plt.ylabel('Restaurant Type', fontsize=14, labelpad=12)
-plt.xlabel('Average Grant Amount', fontsize=14, labelpad=12)
-plt.title('Average Grant Amount by Restaurant Type in CO', fontsize=18, pad=20)
+# Display the average grant amount for each restaurant type at the end of the bar graph
+for i, v in enumerate(grouped_data['count']):
+    offset = grouped_data['mean'].max() * 0.01
+    ax.text(grouped_data['mean'][i] + offset, i - 0.1, '${:,.0f}'.format(grouped_data['mean'][i]), fontsize=12)
+
+
+ax.grid(axis='x', linestyle='--', alpha=0.7)
+ax.set_ylabel('Restaurant Type', fontsize=14, labelpad=12)
+ax.set_xlabel('Average Grant Amount', fontsize=14, labelpad=12)
+ax.set_title('Average Grant Amount by Restaurant Type in CO', fontsize=18, pad=20)
+ax.set_xlim([0, grouped_data['mean'].max() * 1.1])
 
 # Format x-axis as dollar amounts
-ax = plt.gca()
 formatter = ticker.FuncFormatter(lambda x, pos: '${:,.0f}'.format(x))
 ax.xaxis.set_major_formatter(formatter)
 plt.xticks(fontsize=12)
